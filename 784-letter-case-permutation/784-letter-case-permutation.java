e5986038923f0e701fd95e51b5ea8f913b079e84
siwonSeo <siwonst@naver.com>
2022-08-06 15:59:44 +0900

    Time: 7 ms (51.64%), Space: 54.5 MB (29.31%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++){   
+            char c = s.charAt(i);
+            if(!(c >= '0' && c <= '9')) {
+                list.add(i);
+            }
+        }
+        
+        setPermutation(s,list,res,0);        
+        return res;
+    }
+    
+    public void setPermutation(String s,List<Integer> list,List<String> res,int index){
+        
+        if(index == list.size()){
+            res.add(s);
+            return;
+        }
+        char[] cArr = s.toCharArray();
+        /*
+        for(int i = index; i < list.size(); i++){
+            int idx = list.get(i);
+            char c = s.charAt(idx);
+            cArr[idx] = Character.toLowerCase(c);
+            setPermutation(new String(cArr), list, res,index+1);
+            cArr[idx] = Character.toUpperCase(c);
+            setPermutation(new String(cArr), list, res,index+1);            
+        }  
+        */
+        
+        int idx = list.get(index);
+        char c = s.charAt(idx);
+        cArr[idx] = Character.toLowerCase(c);
+        setPermutation(new String(cArr), list, res,index+1);
+        cArr[idx] = Character.toUpperCase(c);
+        setPermutation(new String(cArr), list, res,index+1);     
+        
+    }
+}
