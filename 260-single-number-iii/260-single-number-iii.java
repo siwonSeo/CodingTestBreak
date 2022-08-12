d976f441dd59715340043dd8ab81261d8ca87cb7
siwonSeo <siwonst@naver.com>
2022-08-12 19:19:41 +0900

    Time: 25 ms (5.00%), Space: 48.2 MB (5.22%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+        System.out.println(map);
+        List<Integer> list = new ArrayList<>();
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue() == 1){
+                list.add(entry.getKey());
+            }
+        }
+        System.out.println(list);
+        int[] res = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            res[i] = list.get(i);
+        }
+        
+        return res;
+    }
+}
