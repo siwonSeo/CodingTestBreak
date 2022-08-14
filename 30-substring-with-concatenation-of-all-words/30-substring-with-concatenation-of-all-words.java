7f26b782e2149e74a61ce13bc91abc753ad2f621
siwonSeo <siwonst@naver.com>
2022-08-14 20:53:40 +0900

    Time: 443 ms (19.69%), Space: 118.1 MB (16.16%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,55 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<>();
+        Map<String,Integer> wordMap = getWordsMap(words);
+        System.out.println(wordMap);
+        int wordsLength = words.length;
+        int wordLength = words[0].length();
+        int subStrLength = wordsLength * wordLength;
+        for(int i = 0; i <= s.length()-subStrLength;i++){
+            String subStr = s.substring(i,i+subStrLength);
+            if(getMached(subStr,wordMap,wordLength)){
+                res.add(i);
+                /*
+                while(i+subStrLength+wordLength<s.length()){
+                String preStr = s.substring(i,i+wordLength);
+                String nextPreStr = s.substring(i+subStrLength,i+subStrLength+wordLength);
+                    if(preStr.equals(nextPreStr)){
+                        i+=wordLength;
+                        res.add(i);
+                    }else{
+                        //i+=(subStrLength+wordLength);
+                        break;
+                    }
+                }
+                */
+            }
+        }
+               
+        return res;
+        
+        
+    }
+    
+    public Map<String,Integer> getWordsMap(String[] words){
+        Map<String,Integer> map = new HashMap<>();
+        for(String str : words){
+            map.put(str, map.getOrDefault(str,0)+1);
+        }
+        return map;
+    }
+    
+    public boolean getMached(String s, Map<String,Integer> orgMap,int wordLength){
+        boolean res = true;
+        Map<String,Integer> map = new HashMap<>();
+        for(int i = 0; i<s.length(); i+=wordLength){            
+            String str = s.substring(i,i+wordLength);
+            if(!orgMap.containsKey(str)){
+                return false;
+            }
+            map.put(str, map.getOrDefault(str,0)+1);
+        }
+        System.out.println(map);
+        return orgMap.equals(map);
+    }
+}
