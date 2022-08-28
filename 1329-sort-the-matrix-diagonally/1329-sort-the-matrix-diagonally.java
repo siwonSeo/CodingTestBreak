6bdd41fb5ecfbb997d2164da396be749848234c1
siwonSeo <siwonst@naver.com>
2022-08-28 22:51:41 +0900

    Time: 283 ms (5.03%), Space: 54.7 MB (5.04%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] diagonalSort(int[][] mat) {
+        int yLength = mat.length;
+        int xLength = mat[0].length;
+        for(int i = 0; i < xLength+yLength; i++){
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            int start = Math.max(0,yLength - 1 - i);
+            int to = Math.min(yLength, xLength+yLength - i - 1);            
+            int xIndex = Math.max(0, i - yLength + 1);
+            
+            int tempCnt = 0;
+            for(int j = start; j < to; j++){
+                System.out.println(j);
+                System.out.println(xIndex);
+                System.out.println(tempCnt);
+                pq.add(mat[j][xIndex+tempCnt]);
+                tempCnt++;
+            }
+            tempCnt = 0;
+            for(int j = start; j < to; j++){
+                mat[j][xIndex+tempCnt] = pq.poll();
+                tempCnt++;
+            }              
+          
+        }
+        
+        return mat;
+    }
+    
+
+}
