dbd5b11fced22bc391271852bcf4a0980af21211
siwonSeo <siwonst@naver.com>
2022-09-11 00:47:45 +0900

    Time: 2 ms (90.63%), Space: 41.4 MB (93.27%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -5,10 +5,10 @@ class Solution {
     int[] lowPrice = new int[k+1];
     for(int i=0; i<lowPrice.length; i++) lowPrice[i]=Integer.MAX_VALUE;
     for(int p : prices){
-        for(int i=k; i>=1; i--){
-            maxProfit[i] = Math.max(maxProfit[i],p-lowPrice[i]);
-            lowPrice[i] = Math.min(lowPrice[i],p-maxProfit[i-1]);
-        }
+            for(int i=1; i<=k; i++){
+                maxProfit[i] = Math.max(maxProfit[i],p-lowPrice[i]);
+                lowPrice[i] = Math.min(lowPrice[i],p-maxProfit[i-1]);
+            }
     }
     return maxProfit[k];
     }
