a10257478de34f80f0e91bc796fd3d0ecb312fa5
siwonSeo <siwonst@naver.com>
2022-11-10 23:05:17 +0900

    Time: 0 ms (100.00%), Space: 43 MB (6.36%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] numInfo = new int[3];
+        
+        for(int i = 0; i < nums.length; i++){
+            numInfo[nums[i]]++;
+        }
+        
+        int indexStart = 0;
+        for(int i = 0; i < numInfo.length; i++){
+            int cnt = numInfo[i];
+            
+            for(int j = 0; j < cnt; j++){
+                nums[j+indexStart] = i;
+            }
+            indexStart += cnt;
+        }
+    }
+}
